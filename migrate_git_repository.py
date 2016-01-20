f7782fab1cdd4c14333293afd16121e96a93ff1a
Alexey Kuksin <alexey.kuksin@nomissolutions.com>
2016-01-19 17:23:48 -0800

    added naming arguments.

--- migrate_git_repository.py
+++ migrate_git_repository.py
@@ -1,37 +1,51 @@
 #!/usr/bin/python
 from __future__ import print_function
 
+
 # Script migrates existing git repository to a new remote repository.
 # Follow the prompt's answers. It will use/create local git folder,
 # push existing git branches and tags to the new remote repository
 # and point the local git repository to the new remote repository.
-# If you'd like to run script with input parameters the following order is expected:
-# ./migrate_git_repository.py <repository name> <repository local path> <remote repository url>
-# ./migrate_git_repository.py gitscripts ~/noetl/gitscripts https://github.com/noetl/gitscripts
+# ./migrate_git_repository.py -help
 
-import os, sys, subprocess, re
+
+import os, sys, subprocess, re, argparse, json
 
 
 prompt = '> '
 
 
 def exec_shell(commands):
+
    try:
+
         process = subprocess.Popen('/bin/bash', stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
         out, err =  process.communicate(commands)
+
         print("Stdout: ", out,"\nError:",err,"\n")
+
         return out, err
+
    except:
+
         e = sys.exc_info()
+
         print("exec_shell failed: ", e)
+
         ret = raw_input("Would you like to continue?\n" + prompt)
+
         if re.match(ret, 'yes', re.IGNORECASE):
+
             return None,None
+
         else:
+
             sys.exit(main())
 
 
 def add_path(repopath, command):
+
     return '''
 cd ''' + repopath + ''' +
 ''' + command + '''
@@ -39,77 +53,126 @@ cd ''' + repopath + ''' +
 
 
 def assign_vars(*args):
+
     try:
+
         return str(args[1]),str(args[2]),str(args[3])
+
     except:
+
         return None, None, None
 
 
 class Usage(Exception):
+
     def __init__(self, msg):
+
         self.msg = msg
 
 
 def main(argv=None):
-    global workingdir, reponame, repopath, reporigin, reporemote
 
-    if argv is None:
-        argv = sys.argv
-        print("Lenght of arguments is: ", len(argv) )
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--working_dir", help="working_dir is a local folder where git repository will be created.", default="")
+
+    parser.add_argument("--repo_name", help="repo_name is a git repository name that will be used as a folder name", default="")
+
+    parser.add_argument("--repo_path", help="repo_path is local git repository path if already exists.", default="")
+
+    parser.add_argument("--repo_origin", help="repo_origin is a source origin git repository url", default="")
+
+    parser.add_argument("--repo_remote", help="repo_remote is a remote repository url", default="")
+
+    args = parser.parse_args()
+
+    print("Number of input arguments is: ", len(sys.argv),"\nGiven arguments are: ", json.dumps(vars(args), indent = 4))
+
+    working_dir, repo_name, repo_path, repo_origin, repo_remote = \
+        args.working_dir, args.repo_name, args.repo_path, args.repo_origin, args.repo_remote
 
     try:
-        if len(argv)==4:
-            reponame, repopath, reporemote = assign_vars(*argv)
-        else:
-            reponame = raw_input("Enter git repository name:\n" + prompt)
+        if not (len(repo_name)>0 and len(repo_path)>0 and len(repo_remote)>0):
+
+            repo_name = raw_input("Enter git repository name:\n" + prompt)
+
             ret = raw_input("Would you like to clone migrating repository first? Yes/No:\n" + prompt)
+
             if re.match(ret, 'yes', re.IGNORECASE):
-               workingdir = raw_input("Enter local directory path to clone existing git:\n" + prompt)
-               repopath = workingdir + os.sep + reponame
-               command = "mkdir -p " + repopath
-               out, err = exec_shell(add_path(workingdir,command))
-               reporigin =  raw_input("Enter original git repository url:\n" + prompt)
-               command = "git clone " + reporigin + " " + repopath
-               out, err = exec_shell(add_path(workingdir,command))
+
+               working_dir = raw_input("Enter local directory path to clone existing git:\n" + prompt)
+
+               repo_path = working_dir + os.sep + repo_name
+
+               command = "mkdir -p " + repo_path
+
+               out, err = exec_shell(add_path(working_dir,command))
+
+               repo_origin =  raw_input("Enter original git repository url:\n" + prompt)
+
+               command = "git clone " + repo_origin + " " + repo_path
+
+               out, err = exec_shell(add_path(working_dir,command))
+
             else:
-                repopath = raw_input("Enter local git repository path:\n" + prompt)
-            reporemote = raw_input("Enter remote git repository url in single quotes:\n" + prompt)
+
+                repo_path = raw_input("Enter local git repository path:\n" + prompt)
+
+            repo_remote = raw_input("Enter remote git repository url in single quotes:\n" + prompt)
  
-        print("git repository name: ",reponame, "\ngit repository local path: ",repopath, \
-              "\ngit repository remote url: ", reporemote)
-        out, err = exec_shell(add_path(repopath,"git branch -a"))
+        print("git repository name: ",repo_name, "\ngit repository local path: ",repo_path, \
+              "\ngit repository remote url: ", repo_remote)
+
+        out, err = exec_shell(add_path(repo_path,"git branch -a"))
 
         for line in out.splitlines(True):
+
             if "remotes/origin" in line and "master" not in line:
+
                 command = "git checkout -b " + line.rsplit('/')[-1].rstrip('\n') + " origin/" + line.rsplit('/')[-1].rstrip('\n')
+
                 print("Checkout remote branch: ", command)
-                out, err = exec_shell(add_path(repopath,command))
 
-        print("Adding remote repo", reporemote)
-        command = "git remote add remote-origin " + reporemote
-        out, err = exec_shell(add_path(repopath,command))
+                out, err = exec_shell(add_path(repo_path,command))
 
-        print("Push local branches to remote-origin ", reporemote)
+        print("Adding remote repo", repo_remote)
+
+        command = "git remote add remote-origin " + repo_remote
+
+        out, err = exec_shell(add_path(repo_path,command))
+
+        print("Push local branches to remote-origin ", repo_remote)
+
         command = "git push --all remote-origin"
-        out, err = exec_shell(add_path(repopath,command))
 
-        print("git push tags to remote-origin ", reporemote)
+        out, err = exec_shell(add_path(repo_path,command))
+
+        print("git push tags to remote-origin ", repo_remote)
+
         command = "git push --tags remote-origin"
-        out, err = exec_shell(add_path(repopath,command))
+
+        out, err = exec_shell(add_path(repo_path,command))
 
         print("git remove current origin repository")
+
         command = "git remote rm origin"
-        out, err = exec_shell(add_path(repopath,command))
+
+        out, err = exec_shell(add_path(repo_path,command))
 
         print("git remote rename remote-origin to origin")
+
         command = "git remote rename remote-origin origin"
-        out, err = exec_shell(add_path(repopath,command))
+
+        out, err = exec_shell(add_path(repo_path,command))
 
         print("steps to migrate git repo are done.")
 
     except Usage, err:
+
         print >>sys.stderr, err.msg
+
         print >>sys.stderr, "for help use --help"
+
         return 2
 
 
